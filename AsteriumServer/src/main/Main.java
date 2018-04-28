a6150323390ffd7bf92aa00ab8a79a57ab0a509c
gpschmitt <gschmitt@uw.edu>
2018-04-27 19:56:28 -0700

    Code review and commented AsteriumServer.action, .main, .exceptions

--- AsteriumServer/src/main/Main.java
+++ AsteriumServer/src/main/Main.java
@@ -4,13 +4,25 @@ import java.io.IOException;
 
 import sockettools.ServerSocketListener;
 
+/**
+ * Starts the server.
+ * 
+ * @author Studio Toozo
+ */
 public class Main {
-	
+	// HARD-CODED, this should be dynamic.
 	private static int PORT = 25632;
 
+	/**
+	 * Starts listening on PORT.
+	 * @param args command-line arguments.
+	 */
 	public static void main(String[] args) {
 		try {
+			// Create ServerSocketListener on PORT.
 			ServerSocketListener listener = new ServerSocketListener(PORT);
+			
+			// Start listening.
 			listener.start();
 			
 		} catch (IOException e) {
