7697a18602277143f4e522aed964769505c654bb
Daniel <danielmcbride550@hotmail.com>
2018-05-27 19:18:16 -0700

    nothing

--- AsteriumPlayerClient/AsteriumPlayerClient/public/javascripts/controllers/charactersheetdisplay.js
+++ AsteriumPlayerClient/AsteriumPlayerClient/public/javascripts/controllers/charactersheetdisplay.js
@@ -5,19 +5,20 @@
 function CharacterSheetDisplayController() {
 }
 
-CharacterSheetDisplayController.prototype = new CharacterSheetDisplayController();
-CharacterSheetDisplayController.prototype.constructor = AbstractDisplayController;
+CharacterSheetDisplayController.prototype = Object.create(AbstractDisplayController.prototype);
+CharacterSheetDisplayController.prototype.constructor = CharacterSheetDisplayController;
 
 CharacterSheetDisplayController.prototype.init = function () {
 }
 
 CharacterSheetDisplayController.prototype.display = function () {
     var div = document.getElementById("character");
+    div.style.display = "block";
     div.innerHTML = "";
 
     var nameDiv = document.createElement("div");
     nameDiv.setAttribute("class", "leading");
-    nameDiv.innerHTML = user;
+    nameDiv.innerHTML = "TEST";
     div.appendChild(nameDiv);
 
 }
